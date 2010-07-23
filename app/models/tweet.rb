929bdc8aa35a4791e685178b16dea7dbcd67332a
Marcus Ortiz <mportiz08@gmail.com>
2010-07-23 15:51:25 -0700

    added validations and wrote seed script

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,4 @@
 class Tweet < ActiveRecord::Base
+  validates_presence_of :message, :keyword
+  validates_uniqueness_of :message
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
+require 'twitter-search'
+
+companies = ["apple", "microsoft", "google"]
+companies.each do |company|
+  search = TwitterSearch.new(company)
+  search.all.each do |msg|
+    tweet = Tweet.new(:message => msg, :keyword => company)
+  end
+end
