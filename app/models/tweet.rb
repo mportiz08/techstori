71be3cd5076bf360090b7bbd142f680a17d8697e
Marcus Ortiz <mportiz08@gmail.com>
2010-07-23 15:57:58 -0700

    fixed seed script, working now

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,3 @@
 class Tweet < ActiveRecord::Base
   validates_presence_of :message, :keyword
-  validates_uniqueness_of :message
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,6 @@ companies.each do |company|
   search = TwitterSearch.new(company)
   search.all.each do |msg|
     tweet = Tweet.new(:message => msg, :keyword => company)
+    tweet.save
   end
 end
