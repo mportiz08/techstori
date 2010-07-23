e66a97057c9c3719c7e963625bc7bc070e03e400
Marcus Ortiz <mportiz08@gmail.com>
2010-07-23 16:15:08 -0700

    wrote methods to split message

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,6 @@
 module MainHelper
+  def display(tweet)
+    partition = tweet.split
+    "#{partition.first}<br /><h1>#{tweet.keyword}</h1>#{partition.last}"
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,7 @@
 class Tweet < ActiveRecord::Base
   validates_presence_of :message, :keyword
+  
+  def split
+    message.split(keyword)
+  end
 end
