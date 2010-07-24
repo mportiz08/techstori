c7744c3c74dfa2255450e3cb1089140354a0c2bd
Marcus Ortiz <mportiz08@gmail.com>
2010-07-24 12:02:53 -0700

    working on timing

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
   def index
     @keyword = params[:keyword]
-    @tweets = Tweet.all(:conditions => ["keyword = ?", @keyword], :limit => 5) unless @keyword.nil?
+    @tweets = Tweet.all(:conditions => ["keyword = ?", @keyword]) unless @keyword.nil?
     respond_to do |format|
       format.html
       format.js
