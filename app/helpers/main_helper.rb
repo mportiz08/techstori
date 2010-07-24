8600353a67456102de28fda75e7849e51c13390f
Marcus Ortiz <mportiz08@gmail.com>
2010-07-24 13:41:13 -0700

    added keyword styling

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,6 +1,12 @@
 module MainHelper
   def display(tweet)
     partition = tweet.split
-    "<li>#{partition.first}<br /><h1>#{tweet.keyword}</h1><h3>#{partition.last}</h3></li>"
+    if tweet.keyword.eql?("google")
+      "<li>#{partition.first}<br />
+       <h1 class=\"#{tweet.keyword}\"><span class=\"g\">g</span><span class=\"oe\">o</span><span class=\"o\">o</span><span class=\"g\">g</span><span class=\"l\">l</span><span class=\"oe\">e</span></h1>
+       <h3>#{partition.last}</h3></li>"
+    else
+      "<li>#{partition.first}<br /><h1 class=\"#{tweet.keyword}\">#{tweet.keyword}</h1><h3>#{partition.last}</h3></li>"
+    end
   end
 end
